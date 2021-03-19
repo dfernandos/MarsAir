0b081e0f07fa6805a77d3364c5c76334d85858d7
Daniel Fernando <daniel_fernando@terceiros.sicredi.com.br>
2021-03-19 18:07:18 -0300

    Adiciona baseTest e remove comentários

--- src/test/java/testCase/BookTicketsUsingPromotionlCodeTest.java
+++ src/test/java/testCase/BookTicketsUsingPromotionlCodeTest.java
@@ -1,42 +1,17 @@
 package testCase;
 
-import PageObject.HomePageObject;
+import pageObject.HomePageObject;
 import com.tngtech.java.junit.dataprovider.DataProvider;
 import com.tngtech.java.junit.dataprovider.DataProviderRunner;
 import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import domain.BookTicket;
-import io.github.bonigarcia.wdm.config.DriverManagerType;
-import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
 import org.junit.*;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import steps.BookATicketStep;
+import utils.BaseTest;
 
 @RunWith(DataProviderRunner.class)
-public class BookTicketsUsingPromotionlCodeTestCase {
-
-    static WebDriver driver;
-
-    @BeforeClass
-    public static void beforeClass() {
-        ChromeDriverManager.getInstance(DriverManagerType.CHROME).setup();
-        driver = new ChromeDriver();
-    }
-
-    @Before
-    public void setup() {
-        driver.get("https://marsair.thoughtworks-labs.net/DanielSilva");
-        new WebDriverWait(driver, 15);
-        String title = driver.getTitle();
-        Assert.assertEquals(title, "Mars Airlines: Home");
-    }
-
-    @AfterClass
-    public static void tearDown() {
-        driver.quit();
-    }
+public class BookTicketsUsingPromotionlCodeTest extends BaseTest {
 
     @DataProvider
     public static Object[][] customerDataInfo() {
@@ -52,7 +27,6 @@ public class BookTicketsUsingPromotionlCodeTestCase {
         HomePageObject homePageObject = new HomePageObject(driver);
         homePageObject.getReturningElement().isDisplayed();
         homePageObject.getDepartingElement().isDisplayed();
-
     }
 
     @Test
