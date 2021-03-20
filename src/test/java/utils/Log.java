e1885daad62ae97453e08a15438c38f209e2ec0a
Daniel Fernando <daniel_fernando@terceiros.sicredi.com.br>
2021-03-20 13:06:07 -0300

    Cria classe de log

---
+++ src/test/java/utils/Log.java
@@ -0,0 +1,29 @@
+package utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class Log {
+
+    public Logger logger;
+    FileHandler fileHandler;
+
+    public Log(String fileName) throws SecurityException, IOException{
+        File file = new File(fileName);
+
+        if(!file.exists()){
+            file.createNewFile();
+        }
+
+        fileHandler = new FileHandler(fileName, true);
+        logger = Logger.getLogger("Test");
+        logger.addHandler(fileHandler);
+
+        SimpleFormatter formatter = new SimpleFormatter();
+        fileHandler.setFormatter(formatter);
+
+    }
+}
